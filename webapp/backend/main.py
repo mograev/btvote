6102497031aa3602aeed006f0510811c298c9aa0
Davidoof <38215056+davidoof3141@users.noreply.github.com>
2023-11-24 17:27:40 +0100

    Backend (#3)
    * Added backend structure
    
    * Added prediction request
    
    * Added feature importance + party class
    
    * Added feature importance + party class
    
    * Added random forest
    
    * Added random forest
    
    * Added feature importance + party class
    
    * Added feature importance + party class
    
    * Fixed bugs
    
    * Merged stuff
    
    ---------
    
    Co-authored-by: David <david.le@maven360.io>

---
+++ webapp/backend/main.py
@@ -0,0 +1,57 @@
+import uvicorn as uvicorn
+from fastapi import FastAPI
+import os
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import joblib
+import numpy as np
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import pickle
+
+
+
+##### Pydantic models #####
+class PredictionRequest(BaseModel):
+    type: list
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#### Machine Learning
+#decision_tree = joblib.load('webapp/backend/models/decision_tree.joblib')
+random_forest = joblib.load('webapp/backend/models/random_forest.joblib')
+file = open('webapp/backend/models/classifier.pkl', 'rb')
+classifier = pickle.load(file)
+options = {0: "no", 1: "yes"}
+party = {0: 'AfD', 1: 'CDU', 2: 'CSU', 3: 'FDP', 4: 'GRÜNE', 5: 'Linke', 6: 'SPD'}
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/predict")
+async def predict(body: PredictionRequest):
+    user_response = body.type
+    user_response = np.array(user_response).reshape(1, -1)
+    prediction = classifier.predict_proba(user_response)[0].tolist()
+    return {"prediction": prediction}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        reload=os.getenv("RELOAD", True),
+        port=int(os.getenv("PORT", 8081))
+    )
