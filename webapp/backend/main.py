af3a6be106cda8693ad252b9076cfba6632374f4
David <david.le@maven360.io>
2023-11-26 22:46:34 +0100

    added chart

--- webapp/backend/main.py
+++ webapp/backend/main.py
@@ -33,8 +33,9 @@ app.add_middleware(
 file = open('./models/classifier.pkl', 'rb')
 classifier = pickle.load(file)
 options = {0: "no", 1: "yes"}
-party = {0: 'AfD', 1: 'CDU', 2: 'FDP', 3: 'GRÜNE', 4: 'Linke', 5: 'SPD'}
-results = {i:0 for i in range(6)}
+party = {0: 'AFD', 1: 'CDU/CSU', 2: 'FDP', 3: 'GRÜNE', 4: 'Linke', 5: 'SPD'}
+party_colors = {0: '#1592bf', 1: 'black', 2: 'yellow', 3: '#067808', 4: '#d408bf', 5: 'red'}
+results = {i: 0 for i in range(6)}
 
 @app.get("/")
 async def root():
@@ -48,9 +49,20 @@ async def predict(body: PredictionRequest):
     prediction = classifier.predict_proba(user_response)[0].tolist()
     pred_class = classifier.predict(user_response)[0]
     results[pred_class] += 1
-    values = [value for key, value in results.items()]
-    plt.bar(range(6), values)
-    plt.xticks(range(6), ["AFD", "CDU/CSU", "FDP", "GRÜNE", "LINKE", "SPD"])
+    sorted_result = {k: v for k, v in sorted(results.items(), key=lambda item: item[1], reverse=True)}
+    values = [value for key, value in sorted_result.items()]
+    parties = [party[key] for key in sorted_result]
+    colors = [party_colors[key] for key in sorted_result]
+
+    total = sum(values)
+    percentages = [100 * value / total for value in values]
+
+    plt.bar(range(6), values, color=colors)
+    plt.xticks(range(6), parties)
+
+    for i, (value, percentage) in enumerate(zip(values, percentages)):
+        plt.text(i, value + 0.01, f'{percentage:.2f}%', ha='center')
+
     plt.savefig('./foo.png')
     plt.close()
     return {"prediction": prediction}
@@ -61,5 +73,5 @@ if __name__ == "__main__":
         "main:app",
         host="0.0.0.0",
         reload=os.getenv("RELOAD", True),
-        port=int(os.getenv("PORT", 8081))
+        port=int(os.getenv("PORT", 8082))
     )
